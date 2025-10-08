72bdff2dc1f47b1b2ee3d123df45886a07683d7c
hana <100251756+taku87@users.noreply.github.com>
2025-10-08 16:47:56 +0900

    feat: CORS設定を実装

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # 許可オリジンを環境変数から取得（カンマ区切り）
+    # 例: ALLOWED_ORIGINS="https://example.com,https://partner.com"
+    origins ENV.fetch("ALLOWED_ORIGINS", "http://localhost:3001").split(",")
+
+    resource "/api/*",
+      headers: %w[Authorization Content-Type],
+      methods: [ :get, :post, :options, :head ],
+      credentials: true
+  end
+end
